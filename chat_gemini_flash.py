4afbf18303623c7402fd514b8720a3882a08a0c8
rmerces <ricardo.merces@gmail.com>
2024-11-12 08:27:24 -0300

    add chat_gemini_flash

---
+++ chat_gemini_flash.py
@@ -0,0 +1,26 @@
+import google.generativeai as genai
+import os
+import dotenv
+
+dotenv.load_dotenv()
+ai_model = "gemini-1.5-flash"
+generation_config=genai.types.GenerationConfig(
+        candidate_count=1,
+        temperature=1,
+    )
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel(ai_model, generation_config=generation_config)
+
+def chat_ai(prompt):
+    chat = model.start_chat(
+        history=[
+            {"role": "user", "parts": "Oi, Genai falando!"},
+            {"role": "model", "parts": prompt},
+        ]
+    )
+    response = chat.send_message(prompt)
+    print(response.text)
+
+while True:
+    prompt = input("Prompt: ")
+    chat_ai(prompt)
